696dd4818d6eb1dc3095b97821cf54774697bcfe
Souheil Kooli <kooli_shouheil@yahoo.fr>
2020-12-21 19:56:07 +0100

    path

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -6,8 +6,8 @@ function Profile (props) {
   
     return (
     <div style={{margin:'auto', textAlign:'center'}}>
-      <h1 style={{fontStyle:'italic'}}> Hello, my name is {props.name} </h1>
-      <h2>This is my story: {props.bio} </h2>
+      <h1 style={{fontStyle:'italic'}}> My name is {props.name} </h1>
+      <h2>Bio: {props.bio} </h2>
       <h2>I am a {props.profession} </h2>
       <React.Fragment>
         <img style={{borderRadius: '8px'}} src={props.src} alt=''/>
